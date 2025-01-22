8bfad5afeec0c51f004eeafef51a7ccab6c7e454
ARTHUR CRUZ ALVES <arthur.calves2@senacsp.edu.br>
2025-01-22 17:17:41 -0300

    Operadores

---
+++ fundamentos/operadores.js
@@ -0,0 +1,36 @@
+/**
+ * OPERADORES
+ * Aritméticos
+ * Aritméticos de atribuição
+ * Comparação
+ * Logicos
+ */
+
+console.clear()
+console.log("Operadores aritméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (resto da divisão)")
+console.log("")
+console.log("Operadores aritméticos de atribuição")
+console.log("+= | soma e atribuição a variável")
+console.log("-= | subtração e atribuição a variável")
+console.log("*= | multiplicação e atribuição a variável")
+console.log("/= | divisão e atribuição a variável")
+console.log("++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 ao resultado")
+console.log("")
+console.log("Comparadores")
+console.log(">   | maior que")
+console.log("<   | menor que")
+console.log(">=  | maior ou igual a")
+console.log("<=  | maior ou igual a")
+console.log("=== | idêntico a")
+console.log("!== | não idêntico")
+console.log("")
+console.log("Operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("! | NOT")
